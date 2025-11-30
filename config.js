8f1824f12717203bff4be7cf9c9fe6b9eb724269
miller275 <norok.waihaha@gmail.com>
2025-11-30 20:34:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     
     // Настройки отображения
     DEFAULT_CURRENCY: 'USD',
-    ITEMS_PER_PAGE: 20,
+    ITEMS_PER_PAGE: 10,
     AUTO_REFRESH_INTERVAL: 60000, // 1 минута
     
     // Настройки темы
@@ -21,15 +21,7 @@ const CONFIG = {
     DEFAULT_LANGUAGE: 'ru',
     LANGUAGES: ['ru', 'en'],
     
-    // Ключевые криптовалюты для отслеживания
-    MAJOR_CRYPTOCURRENCIES: [
-        'bitcoin', 'ethereum', 'tether', 'binancecoin', 'solana',
-        'ripple', 'cardano', 'dogecoin', 'polkadot', 'chainlink',
-        'litecoin', 'bitcoin-cash', 'stellar', 'uniswap', 'monero',
-        'ethereum-classic', 'vechain', 'filecoin', 'tezos', 'eos'
-    ],
-    
-    // Настройки Fear & Greed Index
+    // Fear & Greed Index API
     FEAR_GREED_API: 'https://api.alternative.me/fng/',
     
     // Локальное хранилище
@@ -40,8 +32,3 @@ const CONFIG = {
         FAVORITES: 'crypto_dashboard_favorites'
     }
 };
-
-// Экспорт конфигурации
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = CONFIG;
-}
