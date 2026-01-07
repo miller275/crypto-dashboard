02d79b4c64d1cc02e3b67f3428aa4a61cbb3d768
miller275 <norok.waihaha@gmail.com>
2026-01-08 00:39:16 +0200

    Delete assets directory

--- assets/js/core/service-worker.js
+++
@@ -1,6 +0,0 @@
-// Registers the SW for offline caching (static assets + fetched data pages on demand)
-if ("serviceWorker" in navigator){
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js").catch(()=>{});
-  });
-}
